d6353d2a9a1846acd91611ae73b2ce7af3d923e3
Sergey Snozyk <sergey.snozyk@gmail.com>
2023-10-12 17:02:58 +0200

    prepare release
    release-as: 0.1.0

--- mix.exs
+++ mix.exs
@@ -1,13 +1,21 @@
 defmodule Minch.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/nmbrone/minch"
+
   def project do
     [
       app: :minch,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A WebSocket client",
+      source_url: @source_url,
+      homepage_url: @source_url,
+      package: package(),
+      name: "Minch",
+      docs: docs()
     ]
   end
 
@@ -24,6 +32,21 @@ defmodule Minch.MixProject do
       {:cowboy, "~> 2.9", optional: true},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:mint_web_socket, "~> 1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Serhii Snozyk"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Minch",
+      extras: ["CHANGELOG.md"]
     ]
   end
 end
