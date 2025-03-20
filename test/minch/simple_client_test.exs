7e8d16cd9d96180119647230c1e494df04fb0b96
Serhii Snozyk <serhii.snozyk@gmail.com>
2025-03-20 14:08:20 +0100

    feat(TestServer)!: replace cowboy with bandit

--- test/minch/simple_client_test.exs
+++ test/minch/simple_client_test.exs
@@ -3,7 +3,7 @@ defmodule Minch.SimpleClientTest do
 
   setup ctx do
     port = 8882
-    server_state = Map.merge(%{receiver: self(), init_result: :ok}, ctx[:server_state] || %{})
+    server_state = Map.merge(%{receiver: self(), connect_result: :ok}, ctx[:server_state] || %{})
     start_link_supervised!({Server, state: server_state, port: port})
     [url: "ws://localhost:#{port}"]
   end
@@ -12,20 +12,21 @@ defmodule Minch.SimpleClientTest do
     assert {:error, %Mint.TransportError{reason: :nxdomain}} = Minch.connect("ws://example.test")
   end
 
-  @tag server_state: %{init_result: :timeout}
+  @tag server_state: %{connect_result: :timeout}
   test "handles the connection timeout", %{url: url} do
     {:error, :timeout} = Minch.connect(url, [], transport_opts: [timeout: 100])
   end
 
   test "sends headers to the server while connecting", %{url: url} do
     {:ok, _pid, _ref} = Minch.connect(url, [{"x-hello", "world"}])
-    assert_receive {:server, :request, %{headers: %{"x-hello" => "world"}}}
+    assert_receive {:server, :connect, conn}
+    assert Plug.Conn.get_req_header(conn, "x-hello") == ["world"]
   end
 
   test "sends frames to the server", %{url: url} do
     {:ok, pid, _ref} = Minch.connect(url)
     :ok = Minch.send_frame(pid, :ping)
-    assert_receive {:server, :frame, :ping}
+    assert_receive {:server, :frame, {:ping, ""}}
   end
 
   test "sends received frames to the parent process", %{url: url} do
@@ -57,7 +58,7 @@ defmodule Minch.SimpleClientTest do
     {:ok, pid, _ref} = Minch.connect(url)
     monitor_ref = Process.monitor(pid)
     assert_receive {:server, :init, server}
-    Server.send_frame(server, :close)
+    Server.close(server)
     assert_receive {:DOWN, ^monitor_ref, :process, _pid, {:shutdown, _}}
   end
 end
