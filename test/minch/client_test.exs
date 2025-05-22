366cf59e1347231877d7a3bec048079a09f8e679
Serhii Snozyk <serhii.snozyk@gmail.com>
2025-05-22 20:57:56 +0200

    chore(test): use shared server in tests (#46)

--- test/minch/client_test.exs
+++ test/minch/client_test.exs
@@ -1,45 +1,6 @@
 defmodule Minch.ClientTest do
   use ExUnit.Case, async: true
 
-  defmodule Server do
-    use Minch.TestServer
-
-    def send_frame(server, frame) do
-      send(server, {:send_frame, frame})
-      :ok
-    end
-
-    def init(req, state) do
-      send(state.receiver, {:server, :request, req})
-
-      if state.unauthorized do
-        req = :cowboy_req.reply(401, %{}, "Unauthorized", req)
-        {:ok, req, state}
-      else
-        {:cowboy_websocket, req, state}
-      end
-    end
-
-    def websocket_init(state) do
-      send(state.receiver, {:server, :init, self()})
-      {[], state}
-    end
-
-    def websocket_handle(frame, state) do
-      send(state.receiver, {:server, :frame, frame})
-      {[], state}
-    end
-
-    def websocket_info({:send_frame, frame}, state) do
-      {List.wrap(frame), state}
-    end
-
-    def terminate(reason, _req, state) do
-      send(state.receiver, {:server, :terminate, reason})
-      :ok
-    end
-  end
-
   defmodule Client do
     use Minch
 
@@ -49,7 +10,7 @@ defmodule Minch.ClientTest do
 
     def connect(state) do
       send(state.receiver, {:client, :connect, [state]})
-      state.url
+      {state.url, state[:headers] || [], state[:options] || []}
     end
 
     def init(state) do
@@ -90,18 +51,14 @@ defmodule Minch.ClientTest do
   setup ctx do
     port = 8881
     url = "ws://localhost:#{port}"
-    server_state = %{receiver: self(), unauthorized: ctx[:unauthorized]}
+
+    server_state = Map.merge(%{receiver: self(), init_result: :ok}, ctx[:server_state] || %{})
     start_link_supervised!({Server, state: server_state, port: port})
-    {:ok, client} = Client.start_link(%{receiver: self(), url: url})
 
-    server =
-      if server_state.unauthorized do
-        nil
-      else
-        assert_receive {:server, :init, server}
-        server
-      end
+    client_state = Map.merge(%{receiver: self(), url: url}, ctx[:client_state] || %{})
+    {:ok, client} = Client.start_link(client_state)
 
+    assert_receive {:server, :init, server}
     [client: client, server: server, url: url]
   end
 
@@ -114,7 +71,7 @@ defmodule Minch.ClientTest do
     assert_receive {:client, :handle_connect, [%{status: 101, headers: _}, _state]}
   end
 
-  @tag :unauthorized
+  @tag server_state: %{init_result: :unauthorized}
   test "handle_disconnect/2 is called after upgrading error" do
     assert_receive {:client, :handle_disconnect,
                     [%Mint.WebSocket.UpgradeFailureError{status_code: 401}, 1, _]}
@@ -145,10 +102,10 @@ defmodule Minch.ClientTest do
   test "handle_frame/2 is called with all frames", ctx do
     assert_receive {:client, :handle_connect, _}
     Server.send_frame(ctx.server, {:text, "hello"})
-    Server.send_frame(ctx.server, {:binary, <<>>})
+    Server.send_frame(ctx.server, {:binary, ""})
     Server.send_frame(ctx.server, {:pong, "123"})
     assert_receive {:client, :handle_frame, [{:text, "hello"}, _state]}
-    assert_receive {:client, :handle_frame, [{:binary, <<>>}, _state]}
+    assert_receive {:client, :handle_frame, [{:binary, ""}, _state]}
     assert_receive {:client, :handle_frame, [{:pong, "123"}, _state]}
   end
 
@@ -160,7 +117,7 @@ defmodule Minch.ClientTest do
   test "handle_disconnect/2 is called when received a :close frame from server", ctx do
     assert_receive {:client, :handle_connect, _}
     :ok = Server.send_frame(ctx.server, :close)
-    assert_receive {:client, :handle_disconnect, [{:close, 1000, <<>>}, 1, _state]}
+    assert_receive {:client, :handle_disconnect, [{:close, 1000, ""}, 1, _state]}
   end
 
   test "connection is properly closed and terminate/2 is called", ctx do
@@ -179,6 +136,6 @@ defmodule Minch.ClientTest do
     assert_receive {:client, :handle_connect, _}
     assert :ok = Minch.send_frame(ctx.client, :close)
     assert_receive {:server, :terminate, :remote}
-    assert_receive {:client, :handle_disconnect, [{:close, 1000, <<>>}, 1, _state]}
+    assert_receive {:client, :handle_disconnect, [{:close, 1000, ""}, 1, _state]}
   end
 end
---
+++ test/support/server.ex
@@ -0,0 +1,45 @@
+defmodule Server do
+  use Minch.TestServer
+
+  def send_frame(server, frame) do
+    send(server, {:send_frame, frame})
+    :ok
+  end
+
+  def init(req, state) do
+    send(state.receiver, {:server, :request, req})
+
+    case state.init_result do
+      :ok ->
+        {:cowboy_websocket, req, state}
+
+      :unauthorized ->
+        send(state.receiver, {:server, :init, nil})
+        req = :cowboy_req.reply(401, %{}, "Unauthorized", req)
+        {:ok, req, state}
+
+      :timeout ->
+        send(state.receiver, {:server, :init, nil})
+        Process.sleep(:infinity)
+    end
+  end
+
+  def websocket_init(state) do
+    send(state.receiver, {:server, :init, self()})
+    {[], state}
+  end
+
+  def websocket_handle(frame, state) do
+    send(state.receiver, {:server, :frame, frame})
+    {[], state}
+  end
+
+  def websocket_info({:send_frame, frame}, state) do
+    {List.wrap(frame), state}
+  end
+
+  def terminate(reason, _req, state) do
+    send(state.receiver, {:server, :terminate, reason})
+    :ok
+  end
+end
